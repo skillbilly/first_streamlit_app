dd2da0e1a394afdfc5eb1a4554de5ba84a1a33bf
Eric Van Every <vanevery.eric@gmail.com>
2022-04-22 11:36:17 -0700

    Update streamlit_app.py
    Menu change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit
 
-streamlit.title('My Mom\'s New Healthy Diner.')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Bottomless Mimosas')
-streamlit.text('Corned Beef Hash')
-streamlit.text('Chorizo Breakfast burrito')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
