c71cf22884696726fcdc66400fdf8bc733957c80
Eric Van Every <vanevery.eric@gmail.com>
2022-04-22 13:16:15 -0700

    Update streamlit_app.py
    Add drop menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,9 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#insert drop down
+streamlit.multiselect("Pick some fruits:" list(my_fruit_list.index))
+
+#display table data
 streamlit.dataframe(my_fruit_list)
