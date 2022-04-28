e031fec49b227ec9259b00755ae8ef44cf45f575
Eric Van Every <vanevery.eric@gmail.com>
2022-04-28 12:53:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ streamlit.dataframe(my_fruit_list)
 #display fruityvice api response
 
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice =streamlit.text_input('What fruit would you like information about?", 'kiwi')
-streamlit.write("the user entered' + fruit_choice)
+fruit_choice =streamlit.text_input('What fruit would you like information about?', 'kiwi')
+streamlit.write("The user entered' + fruit_choice)
                 
 import requests
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+ fruit_choice)
