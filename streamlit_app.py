07631dfa7f698f8831914f057560d9f078bb873c
Eric Van Every <vanevery.eric@gmail.com>
2022-04-29 09:48:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ def insert_rom_snowflake(new_fruit):
          return streamlit.write('Thanks for adding ' + add_my_fruit)
         
 add_my_fruit =streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-if streamlit.button('Add afruit to the list'):
+if streamlit.button('Add a fruit to the list'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      back_from_function = insert_row_snowflake(add_my_fruit)
      streamlit.text(back_from_function)
