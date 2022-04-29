130afbefbbd11080acbc693d5773ed373ba208fc
Eric Van Every <vanevery.eric@gmail.com>
2022-04-29 09:24:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ streamlit.header('Fruityvice Fruit Advice')
 try:               
   fruit_choice =streamlit.text_input('What fruit would you like information about?' )
   if not fruit_choice:
-      streamlit.error("Please select a fruit t oget information. ")
+      streamlit.error("Please select a fruit to get information. ")
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
@@ -45,19 +45,21 @@ except URLError as e:
       streamlit.error()
       
 
+
+streamlit.header("The fruit load list contains:")
+#Snowflake related functions
+def get_fruit_load_list():
+    with  my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    my_cur.fetchall()    
+ #add button to fruit load
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
 #trouble shooting code below
 streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-
 add_my_fruit =streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding ' + add_my_fruit)
 
