2a252bbce012aba4830a9b9f50282f92bae48dff
Eric Van Every <vanevery.eric@gmail.com>
2022-04-29 09:46:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,21 +51,26 @@ streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
     with  my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
-        return my_cur.fetchall()    
+        return my_cur.fetchall()  
+    
+    
  #add button to fruit load
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-#trouble shooting code below
-streamlit.stop()
+#allow user to add frui to the list
+def insert_rom_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+            return streamlit.write('Thanks for adding ' + add_my_fruit)
+        
 add_my_fruit =streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding ' + add_my_fruit)
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-
+if streamlit.button('Add afruit to the list'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     back_from_function = insert_row_snowflake(add_my_fruit)
+     streamlit.text(back_from_function)
 
 
 
