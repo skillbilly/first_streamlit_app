4bde33a844962ab0ca25e1539e7e6b394dd7131b
Eric Van Every <vanevery.eric@gmail.com>
2022-04-29 08:05:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 
 
 streamlit.header('Breakfast Favorites')
@@ -26,7 +30,7 @@ streamlit.header('Fruityvice Fruit Advice')
 fruit_choice =streamlit.text_input('What fruit would you like information about?', 'kiwi')
 streamlit.write('The user entered' + fruit_choice)
                 
-import requests
+
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+ fruit_choice)
 
                                                    
@@ -41,7 +45,7 @@ streamlit.dataframe(fruityvice_normalized )
 #trouble shooting code below
 streamlit.stop()
 
-import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
